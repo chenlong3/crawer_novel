6e0b9c50a3b30a741005ba66e2ec9b5cefdb9505
cl <605875053>
2017-06-22 18:00:04 +0800

    添加中间件

--- js/app.js
+++ js/app.js
@@ -2,10 +2,22 @@
  * Created by cl on 2017/6/12.
  */
 import express from 'express'
+import path from 'path'
+import logger from 'morgan'
+import cookieParser from  'cookie-parser'
+import bodyParser from 'body-parser'
 import router from './router'
 
 
 const app = express();
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')));
 app.all('*',function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
@@ -19,6 +31,18 @@ app.all('*',function (req, res, next) {
     }
 });
 app.use('/', router);
+app.use(function(req, res, next) {
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
 
 app.listen(process.env.PORT || '3000',function(err){
     if(err)console.log(err);

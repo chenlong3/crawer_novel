3cfa49c347ccea9d505f59278fe0caa4504b728f
cl <605875053>
2017-06-22 16:49:55 +0800

    跨域问题修改请求头

--- js/app.js
+++ js/app.js
@@ -6,6 +6,18 @@ import router from './router'
 
 
 const app = express();
+app.all('*',function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
+    if (req.method === 'OPTIONS') {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+});
 app.use('/', router);
 
 app.listen(process.env.PORT || '3000',function(err){

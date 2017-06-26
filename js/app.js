b7d86c2656455fa076c28010430c86e7a5c49295
cl <605875053@qq.com>
2017-06-26 21:12:28 +0800

    删除node配置静态目录

--- js/app.js
+++ js/app.js
@@ -17,7 +17,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
 app.all('*',function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
